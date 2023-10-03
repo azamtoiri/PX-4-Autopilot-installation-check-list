c17d7b2ead4377dfc53dcb0a5d0fbf2ecb4b15e6
azam <79155097+azamtoiri@users.noreply.github.com>
2023-10-04 00:07:22 +0300

    Update install-PX4-Autopilot.sh

--- install-PX4-Autopilot.sh
+++ install-PX4-Autopilot.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/PX4/PX4-Autopilot --branch=v1.12.3 --recursive
+git clone ~/https://github.com/PX4/PX4-Autopilot --branch=v1.12.3 --recursive
 cd
 bash ./PX4-Autopilot/Tools/setup/ubuntu.sh
 cd PX4-Autopilot
