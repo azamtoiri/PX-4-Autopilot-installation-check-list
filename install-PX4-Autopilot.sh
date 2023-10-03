a1c2f7e968f7ee6a47cdfd71c1c6c3e704c6180b
azam <79155097+azamtoiri@users.noreply.github.com>
2023-10-04 00:04:41 +0300

    Update install-PX4-Autopilot.sh

--- install-PX4-Autopilot.sh
+++ install-PX4-Autopilot.sh
@@ -1,4 +1,5 @@
-git clone ~/https://github.com/PX4/PX4-Autopilot --branch=v1.12.3
+git clone https://github.com/PX4/PX4-Autopilot --branch=v1.12.3 --recursive
+cd
 bash ./PX4-Autopilot/Tools/setup/ubuntu.sh
 cd PX4-Autopilot
 make px4_sitl gazebo_rover -j1
